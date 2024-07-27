53c14752a749d2073da4e93f7352419dfb9af3af
sreerag-13 <sreeragsaji41@gmail.com>
2024-07-27 18:55:50 +0530

    done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,18 +11,19 @@ const Login = () => {
 
         }
     )
-    const inputhandler = (event) => {
-        changedata({ ...sig, [event.target.name]: event.target.value })
+    //value read function
+    const inputhandler=(event) => {
+        changedata({...sig, [event.target.name]: event.target.value})
     }
-    const readvalue = () => {
-        console.log(sig)
-        axios.post("http://localhost:8080/sign",sig).then((response) => {
+    const readvalue=()=> {
+        let newInput={ "Name":sig.Name,"Email":sig.Email,"Password":sig.Password}
+        axios.post("http://localhost:8080/sign",newInput).then((response) => {
             console.log(response.data)
-            if (response.data.status == "success") {
+            if (response.data.status=="success done") {
                 alert("success")
 
             } else {
-                alert("error")
+                alert("email already exist")
 
             }
         }
@@ -45,13 +46,14 @@ const Login = () => {
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Password</label>
-                            <input type="text" className="form-control" name='Password' value={sig.Password} onChange={inputhandler}/>
+                            <input type="password" className="form-control" name='Password' value={sig.Password} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <button className="btn btn-success" onClick={readvalue}>sigin</button>
                             <a href="/signin">register</a>
                             <a href="/">login</a>
                         </div>
+                        
                     </div>
                 </div>
             </div>
